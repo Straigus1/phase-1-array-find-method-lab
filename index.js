5000c0d4db4f3369e3cffbb38ad4d74b0ceafc5b
Dayton <kirylmitch@gmail.com>
2021-11-05 12:30:54 -0400

    commit tree 49fc03a5847487a629cc58571d99adddca5f5757

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 
+function superbowlWin(record) {
+    let score = record.find(obj => obj.result === "W")
+    
+    return score ? score.year : score
+
+    if(score){
+        return score.year
+    } else {
+        return undefined
+    }
+    
+    if(!score) {
+        return undefined
+    } else { 
+        return score.year
+    }
+}
